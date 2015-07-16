044d1e044af9e7ee9e386612fd8732181d82e729
echdevi <devi.ch@ericsson.com>
2015-07-16 09:51:45 +0700

    changes done to fix gson issue

--- cagw-backend/src/main/java/com/ericsson/raso/cac/cagw/web/CagwBackendRestEndpoint.java
+++ cagw-backend/src/main/java/com/ericsson/raso/cac/cagw/web/CagwBackendRestEndpoint.java
@@ -3,22 +3,16 @@ package com.ericsson.raso.cac.cagw.web;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 @Path("/CgWeb/")
 public class CagwBackendRestEndpoint {
 
 	//private static Logger logger = LoggerFactory.getLogger(CagwBackendRestEndpoint.class);
 	@POST
 	@Path("/process/")
-	//@Path("/process/{fe}")
 	//@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	@Consumes("application/json")
 	public void process(byte[] object) {
-		System.out.println("HowManyTimes...");
 		//logger.debug("Reached Inside the process method of CgWeb");
 	}
 }
