e4ed6a6677da7faa51aade5248ed1a571bdb4fd1
echdevi <devi.ch@ericsson.com>
2015-07-23 12:55:11 +0700

    changes done to add alarm

--- cagw-core/src/main/java/com/satnar/common/LogService.java
+++ cagw-core/src/main/java/com/satnar/common/LogService.java
@@ -8,15 +8,9 @@ public class LogService {
 	
 	public static final String STACKTRACE = "cagw_stacktrace";
 	public static final String APPLICATION = "cagw_application";
+	public static final String ALARM = "cagw_alarm";
 	public static Logger stackTraceLog = LoggerFactory.getLogger(STACKTRACE);
 	public static Logger appLog = LoggerFactory.getLogger(APPLICATION);
-		
-	/*public static <T> void debug(Class<T> T,String message){
-		stackTraceLog.debug(T.getName(),message);
-	}
+	public static Logger alarm =  LoggerFactory.getLogger(ALARM);	
 	
-	public static <T> void info(Class<T> T,String message){
-		appLog.info(T.getName(),message);
-	}
-	*/
 }
