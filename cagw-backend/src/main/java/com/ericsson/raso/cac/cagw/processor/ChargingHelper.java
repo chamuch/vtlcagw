e6dcf62481de1f24d994e01c9b5de447d0e62885
Sathya Narayanan Srinivasan <sathya.narayanan.s@hotmail.com>
2015-08-01 13:42:52 +0700

    mapping new OCC result code discovery

--- cagw-backend/src/main/java/com/ericsson/raso/cac/cagw/processor/ChargingHelper.java
+++ cagw-backend/src/main/java/com/ericsson/raso/cac/cagw/processor/ChargingHelper.java
@@ -8,6 +8,7 @@ import com.ericsson.raso.cac.smpp.pdu.viettel.AuthAcc;
 import com.ericsson.raso.cac.smpp.pdu.viettel.WinMoMtFlag;
 import com.ericsson.raso.cac.smpp.pdu.viettel.WinOperationResult;
 import com.satnar.charging.diameter.scap.client.ScapChargingEndpoint;
+import com.satnar.common.LogService;
 import com.satnar.common.charging.diameter.ResultCode;
 
 public class ChargingHelper {
@@ -43,30 +44,48 @@ public class ChargingHelper {
     }
 
     public static WinOperationResult getWinOperationResult(long scapResult, WinMoMtFlag moMtFlag) {
-        if (scapResult == ResultCode.DIAMETER_SUCCESS.getCode() || scapResult == ResultCode.DIAMETER_CREDIT_CONTROL_NOT_APPLICABLE.getCode()) 
+        if (scapResult == ResultCode.DIAMETER_SUCCESS.getCode() || scapResult == ResultCode.DIAMETER_CREDIT_CONTROL_NOT_APPLICABLE.getCode()) { 
+            LogService.appLog.info("Received DIAMETER_SUCCESS or DIAMETER_CREDIT_CONTROL_NOT_APPLICABLE. Assuming success!!");
             return WinOperationResult.SUCCESS;
+        }
         
-        if (scapResult == ResultCode.SUBSCRIBER_NOT_FOUND.getCode())
-            if (moMtFlag == WinMoMtFlag.MO)
+        if (scapResult == ResultCode.SUBSCRIBER_NOT_FOUND.getCode()) {
+            if (moMtFlag == WinMoMtFlag.MO) {
+                LogService.appLog.info("Received SUBSCRIBER_NOT_FOUND. Mapping to MO_USER_ACCT_NOT_EXIST!!");
                 return WinOperationResult.MO_USER_ACCT_NOT_EXIST;
-            else
+            } else {
+                LogService.appLog.info("Received SUBSCRIBER_NOT_FOUND. Mapping to MT_USER_ACCT_NOT_EXIST!!");
                 return WinOperationResult.MT_USER_ACCT_NOT_EXIST;
+            }
+        }
         
         if (scapResult == ResultCode.SUBSCRIBER_GRACE_NOT_ALLOWED.getCode() || 
                 scapResult == ResultCode.SUBSCRIBER_LOCKED.getCode() || 
                 scapResult == ResultCode.SUBSCRIBER_PREACTIVE_NOT_ALLOWED.getCode() || 
-                scapResult == ResultCode.SUBSCRIBER_RECYCLE_NOT_ALLOWED.getCode())
-            if (moMtFlag == WinMoMtFlag.MO)
+                scapResult == ResultCode.SUBSCRIBER_RECYCLE_NOT_ALLOWED.getCode()) {
+            
+            if (moMtFlag == WinMoMtFlag.MO) {
+                LogService.appLog.info("Received SUBSCRIBER_GRACE_NOT_ALLOWED/ SUBSCRIBER_LOCKED/ SUBSCRIBER_PREACTIVE_NOT_ALLOWED/ SUBSCRIBER_RECYCLE_NOT_ALLOWED. "
+                        + "Mapping to MO_USER_ACCT_NOT_CORRECT!!");
                 return WinOperationResult.MO_USER_ACCT_NOT_CORRECT;
-            else
+            } else {
+                LogService.appLog.info("Received SUBSCRIBER_GRACE_NOT_ALLOWED/ SUBSCRIBER_LOCKED/ SUBSCRIBER_PREACTIVE_NOT_ALLOWED/ SUBSCRIBER_RECYCLE_NOT_ALLOWED. "
+                        + "Mapping to MT_USER_ACCT_NOT_CORRECT!!");
                 return WinOperationResult.MT_USER_ACCT_NOT_CORRECT;
+            }
+        }
         
-        if (scapResult == ResultCode.SUBSCRIBER_INSUFFICIENT_BALANCE.getCode())
-            if (moMtFlag == WinMoMtFlag.MO)
+        if (scapResult == ResultCode.SUBSCRIBER_INSUFFICIENT_BALANCE.getCode() || scapResult == ResultCode.DIAMETER_CREDIT_LIMIT_REACHED.getCode()) {
+            if (moMtFlag == WinMoMtFlag.MO) {
+                LogService.appLog.info("Received SUBSCRIBER_INSUFFICIENT_BALANCE/ DIAMETER_CREDIT_LIMIT_REACHED. Mapping to MO_USER_ACCT_NOT_ENOUGH_CREDIT!!");
                 return WinOperationResult.MO_USER_ACCT_NOT_ENOUGH_CREDIT;
-            else
+            } else {
+                LogService.appLog.info("Received SUBSCRIBER_INSUFFICIENT_BALANCE/ DIAMETER_CREDIT_LIMIT_REACHED. Mapping to MT_USER_ACCT_NOT_ENOUGH_CREDIT!!");
                 return WinOperationResult.MT_USER_ACCT_NOT_ENOUGH_CREDIT;
-        
+            }
+        }
+
+        LogService.appLog.info("Received unknown resultCode(" + scapResult + "). Mapping to OTHER_ERRORS!!");
         return WinOperationResult.OTHER_ERRORS;
     }
     
