9e31d70269cea7131c0bf70c76df449baa132c1a
Sathya Narayanan Srinivasan <sathya.narayanan.s@hotmail.com>
2015-08-01 17:21:03 +0700

    aggressive threadpool setting.... lets test

--- cagw-core/src/main/java/com/satnar/smpp/transport/ReadHelper.java
+++ cagw-core/src/main/java/com/satnar/smpp/transport/ReadHelper.java
@@ -29,11 +29,11 @@ public class ReadHelper implements Runnable {
         this.smppConnection = connection;
         this.threadPoolSize = this.smppConnection.getThreadPoolSize();
         
-        this.processorPool = new ThreadPoolExecutor((this.threadPoolSize/4),    // initial pool size
+        this.processorPool = new ThreadPoolExecutor((this.threadPoolSize/3),    // initial pool size
                 this.threadPoolSize,                                            // max pool size
                 300,                                                          // keep alive time
                 TimeUnit.MILLISECONDS,                                          // keep alive time unit (set to 30 secs)
-                new LinkedBlockingQueue<Runnable>((this.threadPoolSize/2)));    // wait queue size
+                new LinkedBlockingQueue<Runnable>((this.threadPoolSize)));    // wait queue size
     }
     
     public void run() {
