261f91d4c44d54b21c31bdda726f75ae3ed97490
echdevi <devi.ch@ericsson.com>
2015-08-05 15:53:12 +0700

    changes done to handle faulty cdrs with incorrect delimeters or incorrect fields

--- cagw-backend/src/main/java/com/ericsson/raso/cac/cagw/dao/SmsScapEdrConsumer.java
+++ cagw-backend/src/main/java/com/ericsson/raso/cac/cagw/dao/SmsScapEdrConsumer.java
@@ -74,65 +74,73 @@ public class SmsScapEdrConsumer {
                     txn.setAmount(amounts);
                     txn.setAccountType(accountTypes);
                     txn.setChargeStatus(true);*/
-                
-                txn = new TransactionDao().fetchSmsCharging(fields[3],fields[0],fields[1]);
-                
-                if(txn != null){
-                	System.out.println("Record found for messageId:"+txn.getMessageId());
-	                txn.setSourceAddress(fields[0]);	                
-	                txn.setDestinationAddress(fields[1]);
-	                txn.setChargedParty(fields[2]);
-	                txn.setMessageId(fields[3]);
-	                txn.setChargingSessionId(fields[5]); 
-	                
-	                //System.out.println("Fields Length:"+fields.length);
-	                
-	                if(fields.length > 6){
-	                	String accounts = "";
-	                    String amounts = "";
-	                    String accountTypes = "";
-	                	String[] daList = fields[6].split(SEMI_COLON);//split with semi colon
-	                	
-	                	for(int i=0; i< daList.length; i++){
-	                        String[] accountInfo = daList[i].split(ACCT_DELIM);
-	                        if (accounts.length() > 0) { accounts += PIPE; }
-	                        
-	                        //25-JUL-2015: To remove prefix "DA"
-	                        if(accountInfo[0].matches("^[DA_PREFIX].*")){
-	                        	accounts += accountInfo[0].replace(DA_PREFIX, "");
-	                        }else{
-	                        	accounts += accountInfo[0];
-	                        }
-	                        
-	                        if (amounts.length() > 0) { amounts += PIPE; }
-	                        
-	                        //25-JUL-2015: As per confirmation from Tanzeem amount to be divided by 1000000
-	                        //and should be multiplied by 100 when sending to AIR - so dividing by 10000
-	                        amounts += Long.valueOf(accountInfo[1])/10000;
-	                        
-	                        if (accountTypes.length() > 0) { accountTypes += PIPE; }
-	                        accountTypes += accountInfo[2];
-	                	}
-	                	txn.setAccountId(accounts);
-	                	txn.setAmount(amounts);
-	                	txn.setAccountType(accountTypes);
-	                	txn.setChargeStatus(true);	                	
-	                    txnHelper.updateSmsCharging(txn);
-	                    System.out.println("Updating sms charging for messageId:"+txn.getMessageId()+":transaction time:"+txn.getTransactionTime() + ":transactionId:"+txn.getTransactionId());
-	                } else {
-	                    //TODO: delete the transaction...	                	
-	                    txn.setChargeStatus(true);
-	                    txnHelper.deleteSmsCharging(txn);
-	                    System.out.println("Updating sms charging for messageId:"+txn.getMessageId()+":transaction time:"+txn.getTransactionTime() + ":transactionId:"+txn.getTransactionId());
-	                }
+                if(fields != null && fields.length < 6){
+                	System.out.println("Insufficient fields or incorrect delimiters - processing stopped at:"+recordEntry);
+                	
+                	try {
+                        new File(csvPath + ".failed").createNewFile();
+                    } catch (IOException e1) {
+                        System.out.println("Unable to create Failed File for:"+csvPath);
+                    }
                 }else{
-                	System.out.println("No record exists with messageid:"+fields[3]+":SourceAddress:"+fields[0]+":DestinationAddress:"+fields[1]);
-                }
+	                txn = new TransactionDao().fetchSmsCharging(fields[3],fields[0],fields[1]);
+	                
+	                if(txn != null){
+	                	System.out.println("Record found for messageId:"+txn.getMessageId());
+		                txn.setSourceAddress(fields[0]);	                
+		                txn.setDestinationAddress(fields[1]);
+		                txn.setChargedParty(fields[2]);
+		                txn.setMessageId(fields[3]);
+		                txn.setChargingSessionId(fields[5]); 
+		                
+		                //System.out.println("Fields Length:"+fields.length);
+		                
+		                if(fields.length > 6){
+		                	String accounts = "";
+		                    String amounts = "";
+		                    String accountTypes = "";
+		                	String[] daList = fields[6].split(SEMI_COLON);//split with semi colon
+		                	
+		                	for(int i=0; i< daList.length; i++){
+		                        String[] accountInfo = daList[i].split(ACCT_DELIM);
+		                        if (accounts.length() > 0) { accounts += PIPE; }
+		                        
+		                        //25-JUL-2015: To remove prefix "DA"
+		                        if(accountInfo[0].matches("^[DA_PREFIX].*")){
+		                        	accounts += accountInfo[0].replace(DA_PREFIX, "");
+		                        }else{
+		                        	accounts += accountInfo[0];
+		                        }
+		                        
+		                        if (amounts.length() > 0) { amounts += PIPE; }
+		                        
+		                        //25-JUL-2015: As per confirmation from Tanzeem amount to be divided by 1000000
+		                        //and should be multiplied by 100 when sending to AIR - so dividing by 10000
+		                        amounts += Long.valueOf(accountInfo[1])/10000;
+		                        
+		                        if (accountTypes.length() > 0) { accountTypes += PIPE; }
+		                        accountTypes += accountInfo[2];
+		                	}
+		                	txn.setAccountId(accounts);
+		                	txn.setAmount(amounts);
+		                	txn.setAccountType(accountTypes);
+		                	txn.setChargeStatus(true);	                	
+		                    txnHelper.updateSmsCharging(txn);
+		                    System.out.println("Updating sms charging for messageId:"+txn.getMessageId()+":transaction time:"+txn.getTransactionTime() + ":transactionId:"+txn.getTransactionId());
+		                } else {
+		                    //TODO: delete the transaction...	                	
+		                    txn.setChargeStatus(true);
+		                    txnHelper.deleteSmsCharging(txn);
+		                    System.out.println("Updating sms charging for messageId:"+txn.getMessageId()+":transaction time:"+txn.getTransactionTime() + ":transactionId:"+txn.getTransactionId());
+		                }
+	                }else{
+	                	System.out.println("No record exists with messageid:"+fields[3]+":SourceAddress:"+fields[0]+":DestinationAddress:"+fields[1]);
+	                }
+	                new File(csvPath + ".success").createNewFile();
+		            successCount = successCount + 1;
+		            System.out.println("Successfully processed the file: " + csvPath); 
+	            }	            
             }
-            new File(csvPath + ".success").createNewFile();
-            successCount = successCount + 1;
-            System.out.println("Successfully processed the file: " + csvPath); 
-            
         } catch (FileNotFoundException e) {        	
             System.out.println("File Not Found: "+csvPath +":" + e.getMessage());
             try {
@@ -156,7 +164,19 @@ public class SmsScapEdrConsumer {
             }
         } catch (PersistenceException e) {
             System.out.println("Processing stopped at entry: " + recordEntry);
-            e.printStackTrace();
+            //e.printStackTrace();
+            try {
+                new File(csvPath + ".failed").createNewFile();
+            } catch (IOException e1) {
+            	System.out.println("Unable to create Failed File for:"+csvPath);
+            }
+        } catch(Exception genE){
+        	System.out.println("Encountered exception while processing file: " + genE.getMessage());
+        	try {
+                new File(csvPath + ".failed").createNewFile();
+            } catch (IOException e1) {
+            	System.out.println("Unable to create Failed File for:"+csvPath);
+            }
         }finally{
         	System.out.println("Success Record Count: " + successCount); 
         	fileInput = null;
