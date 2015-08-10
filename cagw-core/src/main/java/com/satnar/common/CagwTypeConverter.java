01c518d4d4b9493ef7f340ef72fdc8a1f60f695c
Sathya Narayanan Srinivasan <sathya.narayanan.s@hotmail.com>
2015-08-10 18:08:56 +0530

    using non-annotation style converter also for pre-2.16 behaviour check

--- cagw-core/src/main/java/com/satnar/common/CagwTypeConverter.java
+++ cagw-core/src/main/java/com/satnar/common/CagwTypeConverter.java
@@ -3,6 +3,9 @@ package com.satnar.common;
 import org.apache.camel.Converter;
 import org.apache.camel.Exchange;
 import org.apache.camel.FallbackConverter;
+import org.apache.camel.NoTypeConversionAvailableException;
+import org.apache.camel.TypeConversionException;
+import org.apache.camel.TypeConverter;
 
 import com.ericsson.raso.cac.smpp.pdu.viettel.AuthAcc;
 import com.ericsson.raso.cac.smpp.pdu.viettel.AuthAccResponse;
@@ -11,12 +14,16 @@ import com.ericsson.raso.cac.smpp.pdu.viettel.SmResultNotifyResponse;
 import com.google.gson.Gson;
 
 @Converter
-public class CagwTypeConverter {
+public class CagwTypeConverter implements TypeConverter {
     
     private static Gson gson = new Gson();
     
     static {
-        System.out.println(">>>>>>>>>>>> CagwTypeConvertor - Am I getting scanned or not?!!??!!");
+        System.out.println(">>>>>>>>>>>> CagwTypeConvertor - Static - Am I getting scanned or not?!!??!!");
+    }
+    
+    public CagwTypeConverter() {
+        System.out.println(">>>>>>>>>>>> CagwTypeConvertor - Cstr - Am I getting scanned or not?!!??!!");
     }
     
     @Converter
@@ -90,6 +97,67 @@ public class CagwTypeConverter {
         String result = gson.toJson(pojoPayload, SmResultNotifyResponse.class);
         System.out.println(String.format(">>>>>>>>>>>> CagwTypeConvertor - unmarshalling ||JSON: %s||PDU: %s||", result, pojoPayload));
         return result;
+    }
+
+    @Override
+    public boolean allowNull() {
+        return false;
+    }
+
+    @Override
+    public <T> T convertTo(Class<T> type, Object value) throws TypeConversionException {
+        if (type.equals(AuthAcc.class)) {
+            System.out.println(">>>> Converting AuthAcc.. Check 'value': " + value);
+            if (value instanceof String) {
+                System.out.println("AuthAcc: String -> PDU operation");
+                return gson.fromJson(value.toString(), type);
+            } else {
+                System.out.println("Requested type:AuthAcc, but value type: " + value.getClass());
+                throw new TypeConversionException(value, type, new IllegalArgumentException("Requested type:AuthAcc, but value type: " + value.getClass()));
+            }
+        }
+        
+        if (type.equals(SmResultNotify.class)) {
+            System.out.println(">>>> Converting SmResultNotify.. Check 'value': " + value);
+            if (value instanceof String) {
+                System.out.println("SmResultNotify: String -> PDU operation");
+                return gson.fromJson(value.toString(), type);
+            } else {
+                System.out.println("Requested SmResultNotify, but value type: " + value.getClass());
+                throw new TypeConversionException(value, type, new IllegalArgumentException("Requested type:SmResultNotify, but value type: " + value.getClass()));
+            }
+        }
+        throw new TypeConversionException(value, type, new IllegalArgumentException("Requested type:" + type.getName() + ", but value type: " + value.getClass()));
+    }
+
+    @Override
+    public <T> T convertTo(Class<T> type, Exchange exchange, Object value) throws TypeConversionException {
+        System.out.println("Triggered thru convertTo() with exchange method. Requested type:" + type.getName() + ", but value type: " + value.getClass());
+        throw new TypeConversionException(value, type, new IllegalArgumentException("Requested type:" + type.getName() + ", but value type: " + value.getClass()));
+    }
+
+    @Override
+    public <T> T mandatoryConvertTo(Class<T> type, Object value) throws TypeConversionException, NoTypeConversionAvailableException {
+        System.out.println("Triggered thru mandatoryConvertTo() without exchange method. Requested type:" + type.getName() + ", but value type: " + value.getClass());
+        throw new TypeConversionException(value, type, new IllegalArgumentException("Requested type:" + type.getName() + ", but value type: " + value.getClass()));
+    }
+
+    @Override
+    public <T> T mandatoryConvertTo(Class<T> type, Exchange exchange, Object value) throws TypeConversionException, NoTypeConversionAvailableException {
+        System.out.println("Triggered thru mandatoryConvertTo() with exchange method. Requested type:" + type.getName() + ", but value type: " + value.getClass());
+        throw new TypeConversionException(value, type, new IllegalArgumentException("Requested type:" + type.getName() + ", but value type: " + value.getClass()));
+    }
+
+    @Override
+    public <T> T tryConvertTo(Class<T> type, Object value) {
+        System.out.println("Triggered thru tryConvertTo() without exchange method. Requested type:" + type.getName() + ", but value type: " + value.getClass());
+        throw new TypeConversionException(value, type, new IllegalArgumentException("Requested type:" + type.getName() + ", but value type: " + value.getClass()));
+    }
+
+    @Override
+    public <T> T tryConvertTo(Class<T> type, Exchange exchange, Object value) {
+        System.out.println("Triggered thru tryConvertTo() with exchange method. Requested type:" + type.getName() + ", but value type: " + value.getClass());
+        throw new TypeConversionException(value, type, new IllegalArgumentException("Requested type:" + type.getName() + ", but value type: " + value.getClass()));
     }
     
     
